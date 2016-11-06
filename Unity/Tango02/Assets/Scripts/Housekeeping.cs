661b5ff12e1d185d0cad3e71dd9c7a06d3cb6097
Alexander Scheurer <mail@aspepex.net>
2016-11-06 17:34:49 +0100

    Wild tests

---
+++ Unity/Tango02/Assets/Scripts/Housekeeping.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Housekeeping : MonoBehaviour
+    {
+        private float _fullscreenTimer;
+        private readonly float _fullscreenTimeout = 3;
+
+        [UsedImplicitly] public Text Debug1;
+        [UsedImplicitly] public Text Debug2;
+
+        [UsedImplicitly]
+        private void Start ()
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+	
+        [UsedImplicitly]
+        void Update () {
+
+#if UNITY_ANDROID
+            if (Input.touchCount > 0)
+            {
+                Screen.fullScreen = false;
+                _fullscreenTimer = _fullscreenTimeout;
+            }
+
+            if (_fullscreenTimer > 0)
+            {
+                _fullscreenTimer = _fullscreenTimer - Time.deltaTime;
+            }
+            else if (_fullscreenTimer <= 0 && !Screen.fullScreen)
+            {
+                Screen.fullScreen = true;
+            }
+
+            Debug1.text = _fullscreenTimer.ToString(CultureInfo.InvariantCulture);
+            Debug2.text = Screen.fullScreen.ToString();
+#endif
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
